fcad690b00aeb5146c431ee4be0429d6db4ae112
momonomono <momonomono1128@gmail.com>
2022-01-11 16:45:11 +0900

    コードが完成しました

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const https = require("https");
+const line = require("@line/bot-sdk");
 const express = require("express");
 
 const PORT = process.env.PORT || 3000
-const TOKEN = process.env.LINE_ACCESS_TOKEN
 
-const app = express();
 
 const config = {
-    channelSecret : "",
-    channelAccessToken : ""
+    channelSecret: "3b57de0e68aa30e8fbd6516c0474b56e",
+    channelAccessToken: "nYVJ00AE21h1B4ATVhSZqE0EZShWK6CsJyi3YBmeKjsh3IJl5D8WCqFNER/45p3AyllnYN96nwIfF4DF2kZt2FSbAk5Yp733mqZq485G97Bran6DJRfIsPjukwd5C1X8d19u0yMCl4DwUYBRGZ33SQdB04t89/1O/w1cDnyilFU="
 }
+
+
+const app = express();
+const client = new line.Client(config);
+
 
 app.use(express.json())
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }))
 
 app.get("/", (req, res) => {
-  res.sendStatus(200)
+    res.sendStatus(200)
 })
 
-app.post("/webhook", function(req, res) {
-  
+app.post("/callback", line.middleware(config), function (req, res) {
+
+    Promise
+        .all(req.body.events.map(handleEvent))
+        .then((result)=>res.json(result))
 })
+
+async function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text 
+    });
+}
+
+
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
