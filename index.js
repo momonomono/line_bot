0066472b3c3209cb14481981f051e23c41393a41
momonomono <momonomono1128@gmail.com>
2022-01-11 15:44:04 +0900

    必要なところだけ消しました

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const https = require("https")
-const express = require("express")
-const app = express()
+const https = require("https");
+const express = require("express");
+
 const PORT = process.env.PORT || 3000
 const TOKEN = process.env.LINE_ACCESS_TOKEN
+
+const app = express();
+
+const config = {
+    channelSecret : "",
+    channelAccessToken : ""
+}
 
 app.use(express.json())
 app.use(express.urlencoded({
@@ -14,55 +21,7 @@ app.get("/", (req, res) => {
 })
 
 app.post("/webhook", function(req, res) {
-  res.send("HTTP POST request sent to the webhook URL!")
-
-  if (req.body.events[0].type === "message") {
-    // 文字列化したメッセージデータ
-    const dataString = JSON.stringify({
-      replyToken: req.body.events[0].replyToken,
-      messages: [
-        {
-          "type": "text",
-          "text": "Hello, user"
-        },
-        {
-          "type": "text",
-          "text": "May I help you?"
-        }
-      ]
-    })
-
-    // リクエストヘッダー
-    const headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Bearer " + TOKEN
-    }
-
-    // リクエストに渡すオプション
-    const webhookOptions = {
-      "hostname": "api.line.me",
-      "path": "/v2/bot/message/reply",
-      "method": "POST",
-      "headers": headers,
-      "body": dataString
-    }
-
-    // リクエストの定義
-    const request = https.request(webhookOptions, (res) => {
-      res.on("data", (d) => {
-        process.stdout.write(d)
-      })
-    })
-
-    // エラーをハンドル
-    request.on("error", (err) => {
-      console.error(err)
-    })
-
-    // データを送信
-    request.write(dataString)
-    request.end()
-  }
+  
 })
 
 app.listen(PORT, () => {
